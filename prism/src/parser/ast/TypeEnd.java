9202529e712e24232df2bf34dbf634e55b3a25b3
kash <kashishraimallani@gmail.com>
2024-09-30 03:49:38 +0530

    fix

--- prism/src/parser/ast/TypeEnd.java
+++ prism/src/parser/ast/TypeEnd.java
@@ -8,6 +8,9 @@ import parser.visitor.DeepCopy;
 import prism.PrismLangException;
 import prism.PrismTranslationException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TypeEnd extends ProbSessType {
 
     public String toString() {
@@ -18,26 +21,26 @@ public class TypeEnd extends ProbSessType {
         return 1;
     }
 
-    public Module toModule(ExpressionIdent parentRole, ExpressionIdent endVar) throws PrismTranslationException {
-        // set module name
-        Module module = new Module(parentRole.getName());
-        module.setNameASTElement(parentRole);
-        // state variable for parent role
-        String stateVarString = "s_" + parentRole.getName();
-        ExpressionIdent stateVarIdent = new ExpressionIdent(stateVarString);
-        ExpressionLiteral stateVal = new ExpressionLiteral(TypeInt.getInstance(), Integer.valueOf(0));
-        DeclarationInt declType = new DeclarationInt(stateVal, stateVal);
-        module.addDeclaration(new Declaration(stateVarString, declType));
-        // add an end variable to the module
-        String endVarString = endVar.getName();
-        ExpressionLiteral trueVal = new ExpressionLiteral(TypeBool.getInstance(), Boolean.valueOf(true));
-        Declaration endDecl = new Declaration(endVarString, new DeclarationBool());
-        endDecl.setStart(trueVal);
-        module.addDeclaration(endDecl);
-        return module;
+    public Module toModule(
+            ExpressionIdent parentRole,
+            // ExpressionIdent endVar,
+            HashMap<String, Integer> labelsEncoding,
+            int[] numLabels,
+            ArrayList<Expression> sendStates,
+            ArrayList<Expression> pendingStates,
+            HashMap<String, ArrayList<Expression>> endStates
+    ) throws PrismTranslationException {
+        throw new PrismTranslationException("Invalid translation, EndType does not have commands to project");
     }
 
-    public int projectCommands (Module m, int k, int r, ExpressionIdent sessRole, ExpressionIdent endVar, String parent) throws PrismTranslationException {
+    public int projectCommands (Module m, int currentState, int recState, ExpressionIdent parentRole,
+                                //ExpressionIdent endVar,
+                                String parent,
+                                HashMap<String, Integer> labelsEncoding,
+                                int[] numLabels,
+                                ArrayList<Expression> sendStates,
+                                ArrayList<Expression> pendingStates,
+                                HashMap<String, ArrayList<Expression>> endStates) throws PrismTranslationException {
         // This should never be called.
         throw new PrismTranslationException("Invalid translation, EndType does not have commands to project");
     }
